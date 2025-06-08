ee35f51b84ecef7640f9521f2f6deb68dcf52751
kanium3 <dev@kanium.me>
2025-06-08 17:34:03 +0900

    検索欄をタグ入力に対応

--- app/compoent/program/program_input.jsx
+++ app/compoent/program/program_input.jsx
@@ -1,24 +1,54 @@
+import styles from "./program_input.module.css";
 import { Tags } from "@/app/lib/index.js";
-import { useId } from "react";
+import { useId, useState } from "react";
 
 /**
  *
  * @param {import("react").Dispatch<import("react").SetStateAction<Tags>>} onchange
+ * @param {Tags} tags
  * @returns {JSX.Element}
  * @constructor
  */
-export default function ProgramInput({ onchange }) {
+export default function ProgramInput({ onchange, tags }) {
   const programInputId = useId();
+  const tagsAsArray = Array.from(tags.keys());
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <>
+    <div className={styles.inputBox}>
+      {tagsAsArray.map(
+        /**
+         * @param {string} tag
+         * @param {number} index
+         */
+        (tag, index) => {
+          return <TagInputPreview tag={tag} key={index} />;
+        },
+      )}
       <input
         type={"search"}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={(event) => {
-          inputChangeHandler(event, onchange);
+          inputChangeHandler(event, onchange, setInputValue);
         }}
         aria-describedby={programInputId}
       />
-    </>
+    </div>
+  );
+}
+
+/**
+ *
+ * @param {string} tag
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function TagInputPreview({ tag }) {
+  return (
+    <div className={styles.inputPreview}>
+      <p>{tag}</p>
+    </div>
   );
 }
 
@@ -26,12 +56,14 @@ export default function ProgramInput({ onchange }) {
  *
  * @param {KeyboardEvent<HTMLInputElement>} event
  * @param {import("react").Dispatch<import("react").SetStateAction<Tags>>} onchange
+ * @param {import("react").Dispatch<import("react").SetStateAction<string>>} setInput
  */
-function inputChangeHandler(event, onchange) {
+function inputChangeHandler(event, onchange, setInput) {
   if (event.key === "Enter") {
     /** @type {string[]} */
     const init_tags = event.currentTarget.value.split(" ");
     const tags = new Tags(init_tags);
     onchange(tags);
+    setInput("");
   }
 }
