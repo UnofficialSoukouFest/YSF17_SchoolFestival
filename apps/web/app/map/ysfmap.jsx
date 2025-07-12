b46ef4d6e3214d20b81e9849a9e5dc09313a14a8
ameirok <151885371+proirok@users.noreply.github.com>
2025-07-12 07:11:29 +0000

    新たなブランチでオリジナルコントローラーの作成

--- apps/web/app/map/ysfmap.jsx
+++ apps/web/app/map/ysfmap.jsx
@@ -1,7 +1,8 @@
 'use client'
 
+import { useState } from 'react'
 import { CRS, LatLng } from 'leaflet'
-import { MapContainer, LayersControl } from 'react-leaflet'
+import { MapContainer } from 'react-leaflet'
 import { FloorLayer, FloorLayerGroupProvider, PlacePolygon } from '@/app/map/layer'
 
 /* eslint-disable import-x/no-duplicates */
@@ -58,13 +59,51 @@ import Link from 'next/link'
 import programs from '../program.mock.json'
 import { parseProgramsData } from '@latimeria/core'
 
+const items = [
+  { id: 1, item: '1F' },
+  { id: 2, item: '2F' },
+  { id: 3, item: '3F' },
+  { id: 4, item: '4F' },
+  { id: 5, item: '5F' },
+  { id: 6, item: '屋上' },
+]
+
+const RadioButtonItems = propaties =>
+  items.map((item) => {
+    return (
+      <label key={item.id}>
+        <input
+          type="radio"
+          value={item.item}
+          onChange={propaties.handleChange}
+          checked={propaties.checkedValue === item.item}
+        />
+        {item.item}
+      </label>
+    )
+  })
+
+const InputRadio = () => {
+  const [checkedValue, setCheckedValue] = useState(items[0].item)
+  const handleChange = event => setCheckedValue(event.target.value)
+
+  return (
+    <div>
+      <RadioButtonItems
+        handleChange={handleChange}
+        checkedValue={checkedValue}
+      />
+    </div>
+  )
+}
+
 export default function Ysfmap() {
   const programsParse = parseProgramsData(programs)
   const screenWidth = window.innerWidth
   const screenHeight = window.innerHeight
-  const picwidth = screenWidth * 0.6
+  const picwidth = screenWidth >= 1300 ? screenWidth * 0.6 : screenWidth * 0.95
   const picheight = screenHeight * 0.75
-  const displayWidth = screenWidth >= 1300 ? '50vw' : '95vw'
+  const displayWidth = screenWidth >= 1300 ? '60vw' : '95vw'
   const programsList = [...programsParse.iter()]
   /** @type {[{aria:string , item:Program[]}]} */
   return (
@@ -76,33 +115,33 @@ export default function Ysfmap() {
         style={{ width: displayWidth, height: '75vh' }}
         maxBounds={[[0, 0], [picheight, picwidth]]}
       >
-        <LayersControl position="bottomright" collapsed="false">
-          {mapList.map((item) => {
-            return (
-              <LayersControl.BaseLayer checked={item.floor === '1F'} name={item.floor} key={item.floor}>
-                <FloorLayerGroupProvider value={{
-                  src: item.url,
-                  content: item.raw,
-                  picheight: picheight,
-                  picwidth: picwidth }}
-                >
-                  <FloorLayer>
-                    {programsList.filter(content => content.aria.includes(item.floor)).map((content) => {
-                      return (
-                        <PlacePolygon id={content.options.room} pathOptions={{ color: '#0000FF', fillColor: '#0000FFFF', weight: 1 }} key={content.id}>
-                          <Image src={content.options.imagePath} alt="サンプルPR画像" width={100} height={100} key={content.id} />
-                          <Link href={`/program/${content.id}`}>
-                            {content.name}
-                          </Link>
-                        </PlacePolygon>
-                      )
-                    })}
-                  </FloorLayer>
-                </FloorLayerGroupProvider>
-              </LayersControl.BaseLayer>
-            )
-          })}
-        </LayersControl>
+        <InputRadio />
+        {mapList.filter(item => item.floor.includes('1F')).map((item) => {
+          return (
+            <div key={item.floor}>
+              <FloorLayerGroupProvider value={{
+                src: item.url,
+                content: item.raw,
+                picheight: picheight,
+                picwidth: picwidth,
+              }}
+              >
+                <FloorLayer>
+                  {programsList.filter(content => content.aria.includes(item.floor)).map((content) => {
+                    return (
+                      <PlacePolygon id={content.options.room} pathOptions={{ color: '#0000FF', fillColor: '#0000FFFF', weight: 1 }} key={content.id}>
+                        <Image src={content.options.imagePath} alt="サンプルPR画像" width={100} height={100} key={content.id} />
+                        <Link href={`/program/${content.id}`}>
+                          {content.name}
+                        </Link>
+                      </PlacePolygon>
+                    )
+                  })}
+                </FloorLayer>
+              </FloorLayerGroupProvider>
+            </div>
+          )
+        })}
       </MapContainer>
     </div>
   )
